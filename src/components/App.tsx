6af916479d573577785542a49c9aca75bdf76b0e
Code <gsarciotto@gmail.com>
2020-07-03 11:43:52 -0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+function App(): JSX.Element {
+    return <Typography variant="h1">Teste</Typography>;
+}
+
+export default App;
